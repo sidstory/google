7b50942f665233578958b7b68fbfe85aa7b524db
smice <smice_h@qq.com>
2022-12-23 17:38:50 +0800

    优化

--- api/proxy.js
+++ api/proxy.js
@@ -1,28 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
+var target = "https://petalsearch.com/";
 module.exports = (req, res) => {
-  let target = "https://petalsearch.com/";
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
-
 
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    headers:{"LOCALHOST_IP":"66.107.31.223","Proxy-Client-IP":"66.107.31.223","X-Original-Forwarded-For":"66.107.31.223","X-Forwarded-For":"66.107.31.223","x-forwarded-for":"66.107.31.223"},
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/backend`
-      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      //   "^/backend/": "/",
-    },
+    headers:{"LOCALHOST_IP":"66.107.30.220","Proxy-Client-IP":"66.107.30.220","X-Original-Forwarded-For":"66.107.30.220","X-Forwarded-For":"66.107.30.220","x-forwarded-for":"66.107.30.220","Referer":"66.107.30.220"},
   })(req, res);
 };
