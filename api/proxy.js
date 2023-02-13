b209249eb1823b3c0e2965e5d0575d1d3df4f150
smice <31398695+sidstory@users.noreply.github.com>
2023-02-13 09:55:15 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
-    changeOrigin: true,
+    changeOrigin: false,
     headers:{"LOCALHOST_IP":"66.107.30.220","Proxy-Client-IP":"66.107.30.220","X-Original-Forwarded-For":"66.107.30.220","X-Forwarded-For":"66.107.30.220","x-forwarded-for":"66.107.30.220","Referer":"66.107.30.220"},
   })(req, res);
 };
