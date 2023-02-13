688f5ff4210ed425cd9194506ae5489c3d9e086d
smice <31398695+sidstory@users.noreply.github.com>
2023-02-13 10:51:09 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 var target = "https://duckduckgo.com/";
-var security="default-src * 'self';script-src * ‘unsafe-inline’ ‘unsafe-eval’;style-src * ‘unsafe-inline’ ‘unsafe-eval’;img-src * data:;font-src *;connect-src *;manifest-src *";
+var mysecure="default-src * 'self';script-src * ‘unsafe-inline’ ‘unsafe-eval’;style-src * ‘unsafe-inline’ ‘unsafe-eval’;img-src * data:;font-src *;connect-src *;manifest-src *;";
     module.exports = (req, res) => {
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    headers:{"LOCALHOST_IP":"66.107.30.220","Proxy-Client-IP":"66.107.30.220","X-Original-Forwarded-For":"66.107.30.220","X-Forwarded-For":"66.107.30.220","x-forwarded-for":"66.107.30.220","Referer":"66.107.30.220","content-security-policy":security},
+    headers:{"LOCALHOST_IP":"66.107.30.220","Proxy-Client-IP":"66.107.30.220","X-Original-Forwarded-For":"66.107.30.220","X-Forwarded-For":"66.107.30.220","x-forwarded-for":"66.107.30.220","Referer":"66.107.30.220","content-security-policy":mysecure},
   })(req, res);
 };
