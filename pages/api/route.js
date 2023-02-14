124128759aa9ee28b2433a41e78e670fc91673f4
smice <31398695+sidstory@users.noreply.github.com>
2023-02-14 10:43:35 +0800

    Update route.js

--- pages/api/route.js
+++ pages/api/route.js
@@ -1,24 +1,5 @@
-const { createProxyMiddleware, responseInterceptor} = require("http-proxy-middleware");
-var target = "https://start.duckduckgo.com/";
-var mysecure="default-src * blob: data: 'self';script-src * 'unsafe-inline' 'unsafe-eval' blob:;style-src * 'unsafe-inline' 'unsafe-eval' blob: data:;style-src-elem * 'unsafe-inline' data: blob:;img-src * data: blob:;font-src * data: blob:;connect-src *;manifest-src * data: blob:;";
-var head={
-  "LOCALHOST_IP":"66.107.30.220",
-  "Proxy-Client-IP":"66.107.30.220",
-  "X-Original-Forwarded-For":"66.107.30.220",
-  "X-Forwarded-For":"66.107.30.220",
-  "x-forwarded-for":"66.107.30.220",
-  "Referer":"66.107.30.220",
-};
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  createProxyMiddleware({
-    target,
-    selfHandleResponse : true,
-    changeOrigin: true,
-    headers:head,
-    onProxyRes: responseInterceptor(async (buffer, proxyRes, req, res) => {
-      res.setHeader('Content-Security-Policy', mysecure);
-      return buffer.toString();
-    }),
-  })(req, res);
+  res.status(200).json({ name: 'smice' })
 }
