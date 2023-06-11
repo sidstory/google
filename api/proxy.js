a8ad7e0fe49f0183b4a69f5bc742f1d90b6b978a
smice <31398695+sidstory@users.noreply.github.com>
2023-06-11 16:12:32 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,14 @@
 const { createProxyMiddleware, responseInterceptor} = require("http-proxy-middleware");
-var target = "https://www.google.com";
+var target = "https://172.217.163.35";
 var mysecure="default-src * blob: data: 'self';script-src * 'unsafe-inline' 'unsafe-eval' blob:;style-src * 'unsafe-inline' 'unsafe-eval' blob: data:;style-src-elem * 'unsafe-inline' data: blob:;img-src * data: blob:;font-src * data: blob:;connect-src *;manifest-src * data: blob:;";
 var head={
-    "LOCALHOST_IP":"66.107.30.220",
-    "Proxy-Client-IP":"66.107.30.220",
-    "X-Original-Forwarded-For":"66.107.30.220",
-    "X-Forwarded-For":"66.107.30.220",
-    "x-forwarded-for":"66.107.30.220",
-    "Referer":"66.107.30.220",
+    "LOCALHOST_IP":"172.200.163.35",
+    "Proxy-Client-IP":"172.200.163.35",
+    "X-Original-Forwarded-For":"172.200.163.35",
+    "X-Forwarded-For":"172.200.163.35",
+    "x-forwarded-for":"172.200.163.35",
+    "Referer":"172.200.163.35",
+    "Host":"google.ml",
 };
 module.exports = (req, res) => {
     // 创建代理对象并转发请求
